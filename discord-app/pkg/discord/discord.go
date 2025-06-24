97717e00063ef4a496d2a0d5f05c943de449f73d
Simon Linus Schmitt <simon.linus.schmitt@zdh.thm.de>
2025-06-24 03:45:52 +0200

    testing, idk what im doing, vibing half of this

--- discord-app/pkg/discord/discord.go
+++ discord-app/pkg/discord/discord.go
@@ -7,6 +7,7 @@ import (
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/gin-gonic/gin"
 )
 
 func StartBot(token string) error {
@@ -16,19 +17,19 @@ func StartBot(token string) error {
 	}
 
 	// Register the slash command handler
-	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if i.Type == discordgo.InteractionApplicationCommand {
-			switch i.ApplicationCommandData().Name {
-			case "hello":
-				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Content: "Hello from the bot!",
-					},
-				})
-			}
-		}
-	})
+	// dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// 	if i.Type == discordgo.InteractionApplicationCommand {
+	// 		switch i.ApplicationCommandData().Name {
+	// 		case "hello":
+	// 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+	// 				Type: discordgo.InteractionResponseChannelMessageWithSource,
+	// 				Data: &discordgo.InteractionResponseData{
+	// 					Content: "Hello from the bot!",
+	// 				},
+	// 			})
+	// 		}
+	// 	}
+	// })
 
 	err = dg.Open()
 	if err != nil {
@@ -36,14 +37,33 @@ func StartBot(token string) error {
 	}
 	defer dg.Close()
 
-	// Register the slash command
-	_, err = dg.ApplicationCommandCreate(dg.State.User.ID, "", &discordgo.ApplicationCommand{
-		Name:        "hello",
-		Description: "Say hello!",
-	})
-	if err != nil {
-		return fmt.Errorf("cannot create slash command: %w", err)
-	}
+	// _, err = dg.ApplicationCommandCreate(dg.State.User.ID, "", &discordgo.ApplicationCommand{
+	// 	Name:        "hello",
+	// 	Description: "Say hello!",
+	// })
+	// if err != nil {
+	// 	return fmt.Errorf("cannot create slash command: %w", err)
+	// }
+
+	go func() {
+		r := gin.Default()
+		r.POST("/update", func(c *gin.Context) {
+			jwt, err := c.Cookie("jwt")
+			if err != nil || jwt == "" {
+				c.String(401, "Missing or invalid JWT cookie")
+				return
+			}
+			channelID := "1386866380233510962"
+			_, err = dg.ChannelMessageSend(channelID, "Pong from API!")
+			if err != nil {
+				fmt.Printf("Failed to Update Message: %v\n", err)
+				c.String(500, "Failed to update message")
+				return
+			}
+			c.String(200, "Message sent!")
+		})
+		r.Run(":6969")
+	}()
 
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 	stop := make(chan os.Signal, 1)
