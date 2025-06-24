8852492bdcdd2c5efd162d46654290f2b1b55d10
Simon Linus Schmitt <simon.linus.schmitt@zdh.thm.de>
2025-06-24 02:53:34 +0200

    init

---
+++ discord-app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"djms-discord-bot/pkg/discord"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	err = discord.StartBot(os.Getenv("DISCORD_BOT_TOKEN"))
+	if err != nil {
+		log.Fatalf("[DISCORD] Error starting Discord bot: %v", err)
+	}
+	log.Printf("[DISCORD] Discord bot started successfully\n")
+}
