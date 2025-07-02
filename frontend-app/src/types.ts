474d7454921ea486c02d72458fad916b92d2cd3c
Simon Linus Schmitt <simon.linus.schmitt@zdh.thm.de>
2025-07-02 22:38:48 +0200

    add types

---
+++ frontend-app/src/types.ts
@@ -0,0 +1,42 @@
+export interface Event {
+    id: number;
+    name: string;
+    club_id: number;
+}
+
+export interface DancerSlots {
+    event_id: number;
+    club_id: number;
+    dancerslots: Array<DancerSlot>;
+}
+
+export interface DancerSlot {
+    data: number;
+    dancers : Array<Dancer>;
+}
+
+export interface Dancer {
+    id: number;
+    name: string;
+}
+
+export interface TalentSlots {
+    event_id: number;
+    club_id: number;
+    talentslots: Array<TalentSlot>;
+}
+
+export interface TalentSlot {
+    data: number;
+    talents: Array<Talent>;
+}
+
+export interface Talent {
+    id: number;
+    name: string;
+}
+
+export interface Club {
+    id: number;
+    name: string;
+}
