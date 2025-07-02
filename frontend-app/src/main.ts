00c55d75394123203919cc2590af0be99ad40c53
Simon Linus Schmitt <simon.linus.schmitt@zdh.thm.de>
2025-07-02 20:46:39 +0200

    fix formatting i think

--- frontend-app/src/main.ts
+++ frontend-app/src/main.ts
@@ -21,10 +21,10 @@ const app = createApp(App)
 
 registerPlugins(app)
 
-app.use(VueUmamiPlugin, {
+app.use(VueUmamiPlugin({
   websiteID: 'ad1e70c2-9959-473e-8510-be545edd26b4',
   scriptSrc: 'http://praxis.club:3000/script.js',
   router,
-})
+}))
 
 app.use(router).mount('#app')
